b3cdde2baf2aa27451f8db995ec9c2565d293199
alessiopalmieri001 <alessiopalmieri001@gmail.com>
2023-12-13 16:40:12 +0100

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,7 @@ console.log(grid);
 
 // creo un evento al click che mi aggiunge 100 celle con i numeri  da 1 a 100
 bPlay.addEventListener('click', function(){
+        grid.innerHTML = "";
         //creo un ciclo definito per avere dei numeri da 1 a 100
         for(i = 1; i <= 100; i++){
         //creo un tag e lo metto in una variabile
@@ -15,16 +16,18 @@ bPlay.addEventListener('click', function(){
         cella.classList.add('cella');
         //cosi aggiungo dentro le celle i numeri che mi escono 
         cella.append(i);
+        
+        
         //creo un altro evento click per colorare le celle quando si attivano 
         cella.addEventListener('click', function(){
             //con questo comando sto dicendo di prendere il tag della variabile cella e aggiungi la classe active come un interrutore
             this.classList.toggle('active');
-
             console.log(this.innerHTML);
         });
         //qui appendo cioè stampo nel container 
         grid.append(cella);
-    }
 
+    }
+    
 });
 
